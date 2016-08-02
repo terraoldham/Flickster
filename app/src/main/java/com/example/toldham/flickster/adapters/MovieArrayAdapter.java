a34fb0f8ea72b6573d93e2716926fcee85e204f2
Terra Oldham <toldham@hearsaycorp.com>
2016-08-01 21:42:26 -0700

    Simple App Completed

---
+++ app/src/main/java/com/example/toldham/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,50 @@
+package com.example.toldham.flickster.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.toldham.flickster.R;
+import com.example.toldham.flickster.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by toldham on 8/1/16.
+ */
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    public MovieArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+        //check to see if being reused
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        // clear old image
+        imageView.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        //populate data
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(imageView);
+
+        return convertView;
+    }
+}
